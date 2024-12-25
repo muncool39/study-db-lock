479f6e943a3cd35e1388c7ff430b6c4abf5726a7
siwon <muncool39@gmail.com>
2024-12-25 16:16:20 +0900

    [Practice] 데드락

--- com.example.locking/src/main/java/com/example/locking/item/ItemService.java
+++ com.example.locking/src/main/java/com/example/locking/item/ItemService.java
@@ -2,6 +2,7 @@ package com.example.locking.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -36,5 +37,33 @@ public class ItemService {
     @Transactional
     public Item findItemById(Long itemId) {
         return itemRepository.findById(itemId).orElse(null);
+    }
+
+
+    /**
+     * 데드락이 발생하는지 확인하기위한 메서드
+     * @param itemId1 접근할 아이템 1
+     * @param itemId2 접근할 아이템 2
+     * SERIALIZABLE 격리 수준 :
+     *                트랜잭션 간의 완벽한 격리를 보장하기 위해 트랜잭션들이 마치 순차적으로 처리되는 것처럼 보이도록 만든다
+     *                이 수준에서 한 트랜잭션이 데이터를 읽거나 수정하고 있는 동안 다른 트랜잭션은 해당 데이터에 접근할 수 없다
+     * timeout = 1 :
+     *                트랜잭션 시작 후 1초 안에 완료되지 않으면 타임아웃 발생시킴
+     *                데드 락 상황에서 무한히 대기하지 않고 빠져나올 수 있도록 한다
+     */
+    @Transactional(timeout = 1, isolation = Isolation.SERIALIZABLE)
+    public void updateItemsQuantity(Long itemId1, Long itemId2) {
+        // 첫 번째 아이템에 락을 건 후 업데이트
+        Item item1 = itemRepository.findByIdWithLock(itemId1);
+        item1.setQuantity(item1.getQuantity() + 10);
+        itemRepository.save(item1);
+
+        // 데드 락을 발생시키기 위해 잠시 대기
+        try { Thread.sleep(4000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        // 두 번째 아이템에 락을 건 후 업데이트
+        Item item2 = itemRepository.findByIdWithLock(itemId2);
+        item2.setQuantity(item2.getQuantity() + 10);
+        itemRepository.save(item2);
     }
 }
